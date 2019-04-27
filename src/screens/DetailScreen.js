0db7a38e117b64ef3a3929e0c83742ca42954d43
Thomas Petitjean <trolldu25@live.fr>
2019-04-27 22:26:56 +0200

    [Fix] Fix minor pb

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,97 +1,122 @@
-import React, { Component } from 'react';
-import { Text, View, Dimensions } from 'react-native';
-import ComicDescription from '../Components/ComicDescription';
-import Carousel from 'react-native-snap-carousel';
-import { Image } from 'react-native-elements';
+import React, { Component } from "react";
+import { Text, View, Dimensions } from "react-native";
+import ComicDescription from "../Components/ComicDescription";
+import Carousel from "react-native-snap-carousel";
+import { Image } from "react-native-elements";
 
 class DetailScreen extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			url: 'https://gateway.marvel.com:443/v1/public/characters/',
-			apiKey: '?apikey=6a67faed22e2c578c2744e2b0ab8c3fd', //ATTENTION A NE PAS PUSHER
-			datas: {
-				picUrl: 'https://images-na.ssl-images-amazon.com/images/I/41XX8Bzd4vL._SX425_.jpg',
-				title: 'Captain America',
-				description: 'The first avenger'
-			},
-			character: {},
-			charaLoaded: false,
-			comicsLoading: false,
-			comicImgs: [],
-			comics: []
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: "https://gateway.marvel.com:443/v1/public/characters/",
+      apiKey: "?apikey=6a67faed22e2c578c2744e2b0ab8c3fd", //ATTENTION A NE PAS PUSHER
+      datas: {
+        picUrl:
+          "https://images-na.ssl-images-amazon.com/images/I/41XX8Bzd4vL._SX425_.jpg",
+        title: "Captain America",
+        description: "The first avenger"
+      },
+      character: undefined,
+      charaLoaded: false,
+      comicsLoading: false,
+      comicImgs: [],
+      comics: []
+    };
+  }
 
-	componentWillMount() {
-		this.fetchCharacter();
-	}
+  componentDidMount() {
+    this.fetchCharacter();
+  }
 
-	async fetchCharacter() {
-		await fetch(this.state.url + '1011334' + this.state.apiKey, {
-			headers: {
-				Referer: 'localhost'
-			}
-		})
-			.then((response) => response.json())
-			.then((json) => {
-				this.setState({
-					character: json.data.results,
-					charaLoaded: true
-				});
-			});
-	}
+  async fetchCharacter() {
+    let carac = this.props.navigation.getParam("charac");
+    console.log("async", carac.id);
+    await fetch(this.state.url + carac.id + this.state.apiKey, {
+      headers: {
+        Referer: "localhost"
+      }
+    })
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          character: json.data.results,
+          charaLoaded: true
+        });
+      });
+  }
 
-	fetchComic(comics) {
-		this.setState({ comicsLoading: true });
-		comics.map(async (item) => {
-			await fetch(item.resourceURI + this.state.apiKey, {
-				headers: {
-					Referer: 'localhost'
-				}
-			})
-				.then((response) => response.json())
-				.then((json) =>
-					json.data.results[0].images.map((img) => {
-						this.setState({ comicImgs: this.state.comicImgs.concat(img.path + '.' + img.extension) });
-					})
-				);
-		});
-	}
+  fetchComic(comics) {
+    this.setState({ comicsLoading: true });
+    comics.map(async item => {
+      await fetch(item.resourceURI + this.state.apiKey, {
+        headers: {
+          Referer: "localhost"
+        }
+      })
+        .then(response => response.json())
+        .then(json =>
+          json.data.results[0].images.map(img => {
+            this.setState({
+              comicImgs: this.state.comicImgs.concat(
+                img.path + "." + img.extension
+              )
+            });
+          })
+        );
+    });
+  }
 
-	clickBack = () => {
-		this.props.navigation.navigate('Home');
-	};
+  clickBack = () => {
+    this.props.navigation.navigate("Home");
+  };
 
-	_renderItem({ item, index }) {
-		console.log('items', item);
+  _renderItem({ item, index }) {
+    console.log("items", item);
 
-		return (
-			<View>
-				<Image source={{ uri: item }} style={{ width: 250, height: 250 }} />
-			</View>
-		);
-	}
+    return (
+      <View>
+        <Image source={{ uri: item }} style={{ width: 250, height: 250 }} />
+      </View>
+    );
+  }
 
-	render() {
-		return (
-			<View style={{ justifyContent: 'center', alignItems: 'center' }}>
-				<ComicDescription character={this.state.character} />
-				{Object.keys(this.state.character).length != 0 && this.state.comicsLoading === false ? (
-					this.fetchComic(this.state.character[0].comics.items)
-				) : null}
-				<Carousel
-					ref={(c) => {
-						this._carousel = c;
-					}}
-					data={this.state.comicImgs}
-					renderItem={this._renderItem}
-					sliderWidth={Dimensions.get('window').width}
-					itemWidth={Dimensions.get('window').width - 150}
-				/>
-			</View>
-		);
-	}
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.character != this.state.character &&
+      this.state.character != undefined
+    ) {
+      if (
+        Object.keys(this.state.character).length != 0 &&
+        this.state.comicsLoading === false
+      ) {
+        this.fetchComic(this.state.character[0].comics.items);
+      }
+    }
+  }
+
+  render() {
+    console.log("state", this.state.character);
+
+    return (
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <ComicDescription
+          character={
+            this.state.character != undefined ? this.state.character : {}
+          }
+        />
+
+        <Carousel
+          ref={c => {
+            this._carousel = c;
+          }}
+          data={this.state.comicImgs}
+          renderItem={this._renderItem}
+          sliderWidth={Dimensions.get("window").width}
+          itemWidth={Dimensions.get("window").width - 150}
+        />
+      </View>
+    );
+  }
 }
 
 export default DetailScreen;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,139 +1,151 @@
-import React, { Component } from 'react'
-import { View, ScrollView, Dimensions, StyleSheet } from 'react-native'
-import { Text, Searchbar, ActivityIndicator } from 'react-native-paper';
-import SuperHeroList from '../components/SuperHeroList';
+import React, { Component } from "react";
+import { View, ScrollView, Dimensions, StyleSheet } from "react-native";
+import { Text, Searchbar, ActivityIndicator } from "react-native-paper";
+import SuperHeroList from "../Components/SuperHeroList";
 
-class HomeScreen extends Component {
+export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        offset:0,
-        listCharac: [],
-        query: '',
-        isSearchActive: false,  
-        isLoading: true
+      offset: 0,
+      listCharac: [],
+      query: "",
+      isSearchActive: false,
+      isLoading: true
     };
   }
 
-  handleInputChange = (searchValue) => {
+  handleInputChange = searchValue => {
     var emptyList = [];
     if (searchValue === undefined || searchValue === "") {
-      this.setState({
-        isSearchActive: false,
-        offset: 0,
-        query: searchValue,
-        listCharac: emptyList
-      }, () => {        
-        this.getAllHeroes();
-      })
+      this.setState(
+        {
+          isSearchActive: false,
+          offset: 0,
+          query: searchValue,
+          listCharac: emptyList
+        },
+        () => {
+          this.getAllHeroes();
+        }
+      );
     } else {
-      this.setState({
-        query: searchValue,
-        isSearchActive: true
-      }, () => {
-        this.getHeroesByQuery();
-      }) 
+      this.setState(
+        {
+          query: searchValue,
+          isSearchActive: true
+        },
+        () => {
+          this.getHeroesByQuery();
+        }
+      );
     }
-  }
+  };
 
   // REQUETES API
   componentDidMount() {
+    console.log("cdm");
     this.getAllHeroes();
   }
 
-  getHeroesByQuery(){
-    fetch('https://gateway.marvel.com/v1/public/characters?nameStartsWith=' + this.state.query + '&apikey=02ee3d93e987a3a5be62d77604e7d8da' + '&limit=30', {
-      headers: {
-        Referer: 'localhost'
+  getHeroesByQuery() {
+    fetch(
+      "https://gateway.marvel.com/v1/public/characters?nameStartsWith=" +
+        this.state.query +
+        "&apikey=02ee3d93e987a3a5be62d77604e7d8da" +
+        "&limit=30",
+      {
+        headers: {
+          Referer: "localhost"
+        }
       }
-    }).then(responseJson =>
-      responseJson.json()
- ).then(json => 
-  this.setState({
-    listCharac: json.data.results
-  })
-  )
-}
+    )
+      .then(responseJson => responseJson.json())
+      .then(json =>
+        this.setState({
+          listCharac: json.data.results
+        })
+      );
+  }
 
-getAllHeroes() {
-  console.log("getAllHeroes")
-  fetch('https://gateway.marvel.com/v1/public/characters?apikey=02ee3d93e987a3a5be62d77604e7d8da' + "&offset=" + this.state.offset, {
-    headers: {
-      Referer: 'localhost'
-    }
-  }).then(responseJson =>
-     responseJson.json()
-).then(json =>
-  this.setState({
-    isLoading: false,      
-    listCharac: this.state.listCharac ? this.state.listCharac.concat(json.data.results) : json.data.results
-  })
-);
-}
+  getAllHeroes() {
+    console.log("getAllHeroes");
+    fetch(
+      "https://gateway.marvel.com/v1/public/characters?apikey=02ee3d93e987a3a5be62d77604e7d8da" +
+        "&offset=" +
+        this.state.offset,
+      {
+        headers: {
+          Referer: "localhost"
+        }
+      }
+    )
+      .then(responseJson => responseJson.json())
+      .then(json =>
+        this.setState({
+          isLoading: false,
+          listCharac: this.state.listCharac
+            ? this.state.listCharac.concat(json.data.results)
+            : json.data.results
+        })
+      );
+  }
 
-loadMore() {
-  if (this.state.isSearchActive === false) {
+  loadMore() {
+    if (this.state.isSearchActive === false) {
       this.getAllHeroes();
 
       this.setState({
         offset: this.state.offset + 20
-      })
+      });
+    }
   }
-  
-}
-
 
   render() {
     var styles = StyleSheet.create({
       loader: {
-          width: Dimensions.get('window').width,
-          height: Dimensions.get('window').height,
-          flex:1,
-          alignItems:'center',
-          justifyContent: 'center'
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height,
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
       }
-    })
+    });
 
     var queryHasResults;
-    if(this.state.listCharac.length === 0) {
+    if (this.state.listCharac.length === 0) {
       queryHasResults = false;
     } else {
       queryHasResults = true;
     }
 
-    if (this.state.isLoading){
-      return <ActivityIndicator animating={true} style={styles.loader} />
-   }
-      
+    if (this.state.isLoading) {
+      return <ActivityIndicator animating={true} style={styles.loader} />;
+    }
+
     return (
       <ScrollView stickyHeaderIndices={[0]}>
-
         <View>
-            <Searchbar
-              placeholder="Rechercher"
-              onChangeText={ query => { this.handleInputChange(query); }  }
-              ref={input => this.search = input}
-            />
+          <Searchbar
+            placeholder="Rechercher"
+            onChangeText={query => {
+              this.handleInputChange(query);
+            }}
+            ref={input => (this.search = input)}
+          />
         </View>
 
         <SuperHeroList
-        list = {this.state.listCharac}
-        navigation = {this.props.navigation}
-        onPress = {() => this.props.navigation.navigate('Details', {
-          charac: charac
-         })}
+          list={this.state.listCharac}
+          navigation={this.props.navigation}
         />
-        
-        {queryHasResults === false &&
-            <View style={styles.loader}>
-              <Text>Aucun résultat trouvé</Text>
-            </View>
-        }
 
+        {queryHasResults === false && (
+          <View style={styles.loader}>
+            <Text>Aucun résultat trouvé</Text>
+          </View>
+        )}
       </ScrollView>
-    )
+    );
   }
-
 }
-
-export default HomeScreen;
